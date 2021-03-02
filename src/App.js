812a07ddf5f250995d7244cbe9ce421cade8388d
Teo Marcelo <tmarcelojr@gmail.com>
2021-03-02 14:36:15 -0600

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
+import React, {useEffect} from 'react'
 import './App.css';
 
 function App() {
+  console.log('initial')
+  useEffect(() => {
+    async function getCompanyReviews() {
+      console.log('we getting this')
+      try{
+        const reviewsRes = await fetch('http://localhost:8000/api/v1/reviews')
+        const reviewsJson = await reviewsRes.json()
+        console.log(reviewsJson)
+        // setUserReviews(reviewsJson.data)
+      } catch(err) {
+        console.log(err);
+      }
+    }
+    getCompanyReviews()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
